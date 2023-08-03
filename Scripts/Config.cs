2961eeda3c24d8a1d843a42ace6ac34763c6dbed
azirdadev <140871462+azirdadev@users.noreply.github.com>
2023-08-03 18:12:26 -0400

    Update Config.cs

--- Scripts/Config.cs
+++ Scripts/Config.cs
@@ -12,14 +12,12 @@ namespace ConfigMOTD.Scripts
         public static ConfigFile ConfigFile { get; private set; }
 
         public static ConfigEntry<string> TopText;
-        public static ConfigEntry<string> MainText;
 
         public static void Initalize()
         {
             ConfigFile = new ConfigFile(Path.Combine(Paths.ConfigPath, "MOTD.cfg"), true);
 
-            TopText = ConfigFile.Bind("Configuration", "Header Text", "MESSAGE OF THE DAY", "This will change the main header text of the Top Text");
-            MainText = ConfigFile.Bind("Configuration", "Body Text", "TEXT HERE", "This will change the body text of MOTD Board");
+            TopText = ConfigFile.Bind("Configuration", "Header Text", "MESSAGE OF THE DAY", "This will change the Top text");
 
         }
     }
